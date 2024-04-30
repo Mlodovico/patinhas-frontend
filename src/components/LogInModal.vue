d481257d868b4132b16021f0bf8b635f923d7c3d
mlodovico <murilo.lodovico@outlook.com>
2024-04-30 18:56:58 -0300

    feature: signIn modal styles

--- src/components/LogInModal.vue
+++ src/components/LogInModal.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="logInModal" @click="handleClickLogin">
-        <div class="modal"></div>
+        <div class="modal">
+            <div class="close-button">
+                <p>Fechar</p>
+            </div>
+            <div class="form">
+                <h1>Acessar sua conta</h1>
+                <p>E-mail ou CPF</p>
+                <input type="text">
+                <p>Senha</p>
+                <input type="password">
+                <button>Entrar</button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -26,10 +38,42 @@ const handleClickLogin = () => {
     background: rgba(0, 0, 0, 0.5);
 }
 
+.close-button {
+    display: flex;
+    justify-content: flex-end;
+    margin: 10px;
+}
+
 .modal {
-    width: 70%;
-    height: 70%;
     background: white;
     border-radius: 12px;
+    color: black;
+}
+
+.form {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 5rem;
+}
+
+.form input {
+    margin-top: 10px;
+    border-radius: 4px;
+    border: 1px solid gray;
+    padding: 8px;
+}
+
+.form button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 50px;
+    margin-top: 10px;
+    border-radius: 4px;
+    border: 1px solid red;
+    background: red;
 }
 </style>
